fb45578862ee554dadffafaf56bca185d61a6251
sunwn0104 <62931996+sunwn0104@users.noreply.github.com>
2022-08-31 20:40:45 +0800

    Update Test.java

--- src/main/java/com/sunwn/git/test/Test.java
+++ src/main/java/com/sunwn/git/test/Test.java
@@ -10,8 +10,5 @@ public class Test {
         System.out.println("hello word1");
         System.out.println("hello word2");
         System.out.println("hello word3");
-        System.out.println("hello word4");
-        System.out.println("hello master");
-        System.out.println("hello hot-fix");
     }
 }
