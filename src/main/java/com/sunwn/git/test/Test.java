84d8a106a96282144e0deb2cddd1ad37ede30ff8
sunwn <987828872@qq.com>
2022-08-31 18:19:57 +0800

    test1

--- src/main/java/com/sunwn/git/test/Test.java
+++ src/main/java/com/sunwn/git/test/Test.java
@@ -7,6 +7,6 @@ package com.sunwn.git.test;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello word");
     }
 }
